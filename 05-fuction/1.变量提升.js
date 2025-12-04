b2c7c082d0e40cae5ec539db5851b776c9932b5b
zhuym <yiming.zhuym@dtzhejiang.com>
2025-12-04 15:34:24 +0800

    feat:函数的执行机制

---
+++ 05-fuction/1.变量提升.js
@@ -0,0 +1,30 @@
+// console.log(fn);
+// function fn() {
+//   console.log("hello world");
+// }
+// console.log(fn);
+// var fn = 12;
+// console.log(fn);
+// function fn() {
+//   console.log("hello world2");
+// }
+// console.log(a);
+// if (!("a" in window)) {
+//   var a = 12;
+// }
+// console.log(a);
+
+console.log(a);
+console.log(b);                                      //undefined          12                                          14                                                    15                                                                                                                                                                                                                                                                                                                                                                                      
+var a = 12;
+let b = 13;
+if (1 == 1) {
+  console.log(a);
+  console.log(b);
+  var a = 14;
+  let b = 15;
+  console.log(a);
+  console.log(b);
+}
+console.log(a);
+console.log(b);
